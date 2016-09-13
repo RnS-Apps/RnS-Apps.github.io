ad2d7f732a9515dd5a729588287803393189767f
Sukh Kalsi <SukhKalsi@users.noreply.github.com>
2016-09-13 09:27:04 +0100

    Initial commit based on theme.

---
+++ js/agency.js
@@ -0,0 +1,42 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        var scrollTop = $($anchor.attr('href')).offset().top;
+        if (scrollTop) {
+            scrollTop -= 80;
+        }
+        $('html, body').stop().animate({
+            scrollTop: scrollTop
+        }, 1800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 80,
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
